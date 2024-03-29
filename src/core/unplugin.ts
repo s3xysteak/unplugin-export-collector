43400daa8a875fbfe483c90e40a8a47b5e08753e
s3xysteak <j.yulia@qq.com>
2024-03-29 16:09:53 +0800

    feat(unplugin): Support both awaitable function param and object param.

--- src/core/unplugin.ts
+++ src/core/unplugin.ts
@@ -1,6 +1,7 @@
 import process from 'node:process'
 import { createUnplugin } from 'unplugin'
 import { resolve } from 'pathe'
+import { type Callable, toPromise } from '@s3xysteak/utils'
 import { addExtension } from './utils'
 import { type ExpGeneratorOptions, expGenerator } from './generator'
 
@@ -8,15 +9,20 @@ export interface UnpluginFactoryOptions extends ExpGeneratorOptions {
   entries: string[]
 }
 
-export default createUnplugin<Partial<UnpluginFactoryOptions> | undefined>((options = {}) => {
-  const {
-    entries = ['./src/index.ts'],
-  } = options
-  const _entries = entries.map(addExtension).map(entry => resolve(process.cwd(), entry))
-
+export default createUnplugin<Callable<Partial<UnpluginFactoryOptions>> | undefined>((options = {}) => {
   return {
     name: 'unplugin-auto-import-generator',
     async buildStart() {
+      options = await toPromise(options)
+
+      const {
+        entries = ['./src/index.ts'],
+      } = options
+
+      const _entries = entries
+        .map(addExtension)
+        .map(entry => resolve(process.cwd(), entry))
+
       for (const entry of _entries)
         await expGenerator(entry, options)
     },
